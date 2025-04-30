3b0ae413c6a3ff37789b8795b9b711c5a0406ff3
Rma816 <rocmoragu@alum.us.es>
2025-04-30 11:15:49 +0200

    Initial commit

---
+++ src/main/java/aiss/proyecto/gitminer/ProyectoGitMinerApplication.java
@@ -0,0 +1,13 @@
+package aiss.proyecto.gitminer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoGitMinerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoGitMinerApplication.class, args);
+	}
+
+}
