e222c5d6a10f7666f05959da2b3f36f80f85fec6
Miguel <migcuesan@alum.us.es>
2025-05-10 15:06:32 +0200

    Bitbucket-userService(No use)

--- bitbucketminer/src/main/java/aiss/bitbucketminer/service/UserService.java
+++ bitbucketminer/src/main/java/aiss/bitbucketminer/service/UserService.java
@@ -1,4 +1,11 @@
 package aiss.bitbucketminer.service;
 
+import aiss.bitbucketminer.model.BitBucketUser;
+import org.springframework.web.client.RestTemplate;
+
 public class UserService {
+    public static BitBucketUser getUserFromUrl(String url) {
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate.getForObject(url, BitBucketUser.class);
+    }
 }
